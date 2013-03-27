27b038513da703e6fc6c0ec7aad3451d761eaaae
Thiago Fernandes Massa <thiagown@gmail.com>
2013-03-27 18:59:37 -0300

    Added structure for the webapp.

--- lib/webapp.rb
+++ lib/webapp.rb
@@ -1,4 +1,14 @@
+# Sinatra
+require 'sinatra/base'
+require 'sinatra/contrib'
+require 'json'
+
 module MemcachedManager
   class Webapp < Sinatra::Base
+    enable :sessions
+
+    get '/' do
+      erb :index
+    end
   end
 end
