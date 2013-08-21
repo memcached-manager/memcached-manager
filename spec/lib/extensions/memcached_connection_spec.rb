84f0c766741982684646770880e905a983357bf6
Thiago Fernandes Massa <thiagown@gmail.com>
2013-08-20 23:39:21 -0300

    Updated tests with new matcher

--- spec/lib/extensions/memcached_connection_spec.rb
+++ spec/lib/extensions/memcached_connection_spec.rb
@@ -4,19 +4,15 @@ describe Sinatra::MemcachedConnection do
   let(:klass) { Class.new.extend(Sinatra::MemcachedConnection) }
 
   context "#setup_memcached" do
-    it "should setup the memcached instance variable" do
-      klass.setup_memcached 'localhost', '1337'
-      klass.instance_variables.should include :"@memcached"
-    end
+    before(:each) { klass.setup_memcached 'localhost', '1337' }
+    it { klass.should have_instance_variable(:memcached) }
   end
 
   context "#close_memcached" do
     before(:each) { klass.setup_memcached 'localhost', '1337' }
 
-    it do
-      # There isn't an Dalli api that tells if the connection is closed
-      klass.respond_to? :close_memcached
-    end
+    # There isn't an Dalli api that tells if the connection is closed
+    it { klass.respond_to? :close_memcached }
   end
 
   context "#memcached_connection" do
