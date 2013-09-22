4bf0be0126fbb659e879658d3f6eac090c8d2b48
Thiago Fernandes Massa <thiagown@gmail.com>
2013-09-22 17:39:59 -0300

    Testcase for notification service

---
+++ spec/javascripts/angular/services/notification_spec.js
@@ -0,0 +1,45 @@
+describe("Factory Notification", function() {
+  var service;
+  var message;
+
+  beforeEach(function(){
+    module('keysServices');
+    inject(function(Notification){
+      service = Notification;
+    });
+    message = "Some random message";
+  });
+
+  describe("alert method", function(){
+    it("notificates the user with an alert", function(){
+      noty = jasmine.createSpy('noty');
+      service.alert(message);
+      expect(noty).toHaveBeenCalledWith({text: message, type: 'alert'});
+    });
+  });
+
+  describe("error method", function(){
+    it("notificates the user with an error", function(){
+      noty = jasmine.createSpy('noty');
+      service.error(message);
+      expect(noty).toHaveBeenCalledWith({text: message, type: 'error'});
+    });
+  });
+
+  describe("info method", function(){
+    it("notificates the user with an info", function(){
+      noty = jasmine.createSpy('noty');
+      service.info(message);
+      expect(noty).toHaveBeenCalledWith({text: message, type: 'information'});
+    });
+  });
+
+  describe("success method", function(){
+    it("notificates the user with as success", function(){
+      noty = jasmine.createSpy('noty');
+      service.success(message);
+      expect(noty).toHaveBeenCalledWith({text: message, type: 'success'});
+    });
+  });
+
+});
