28c3152b25bcbd044de11e6d7b2c4dc681c7f303
Thiago Fernandes Massa <thiagown@gmail.com>
2015-01-04 20:18:24 -0200

    Fixed build

--- features/step_definitions/api/run_command.rb
+++ features/step_definitions/api/run_command.rb
@@ -6,5 +6,5 @@ When(/^I post the command "(.*?)" to "(.*?)" in the API$/) do |command, route|
 end
 
 Then(/^it should be run on memcached and give a response$/) do
-  expect(JSON.parse(@response.body)).to eq({"response"=>"\"VALUE ineed 1 20\\n\\u0004\\bI\\\"\\u000Ftogetalife\\u0006:\\u0006ET\\nEND\\n\""})
+  expect(JSON.parse(@response.body)).to eq {"response"=>"\"VALUE ineed 1 20\\n\\x04\\bI\\\"\\x0Ftogetalife\\x06:\\x06ET\\nEND\\n\""}
 end
